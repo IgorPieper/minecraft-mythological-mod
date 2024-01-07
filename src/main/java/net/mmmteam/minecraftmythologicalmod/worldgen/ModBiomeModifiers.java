091e899f87c56f7fe92aee5ea57ad7265f88ba74
Igor Pieper <91740800+IgorPieper@users.noreply.github.com>
2024-01-07 01:18:38 +0100

    Paczka Zmian 4

--- src/main/java/net/mmmteam/minecraftmythologicalmod/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/mmmteam/minecraftmythologicalmod/worldgen/ModBiomeModifiers.java
@@ -1,5 +1,6 @@
 package net.mmmteam.minecraftmythologicalmod.worldgen;
 
+import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.mmmteam.minecraftmythologicalmod.MinecraftMythologicalMod;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
@@ -11,21 +12,28 @@ import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.world.BiomeModifier;
 import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.mmmteam.minecraftmythologicalmod.entity.ModEntities;
+
+import java.util.List;
 
 public class ModBiomeModifiers {
     public static final ResourceKey<BiomeModifier> ADD_TREE_OLIVE = registerKey("add_tree_olive");
 
     public static final ResourceKey<BiomeModifier> ADD_IRIS = registerKey("add_iris");
     public static final ResourceKey<BiomeModifier> ADD_ACONITE = registerKey("add_aconite");
-
     public static final ResourceKey<BiomeModifier> ADD_LAUREL = registerKey("add_laurel");
+
+    public static final ResourceKey<BiomeModifier> SPAWN_RACOON = registerKey("spawn_racoon");
+    public static final ResourceKey<BiomeModifier> SPAWN_CYCLOP = registerKey("spawn_cyclop");
+    public static final ResourceKey<BiomeModifier> SPAWN_LYNX = registerKey("spawn_lynx");
+    public static final ResourceKey<BiomeModifier> SPAWN_MINOTAUR = registerKey("spawn_minotaur");
 
     public static void bootstrap(BootstapContext<BiomeModifier> context) {
         var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
 
         context.register(ADD_TREE_OLIVE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
-                biomes.getOrThrow(Tags.Biomes.IS_MUSHROOM),
+                biomes.getOrThrow(Tags.Biomes.IS_MOUNTAIN),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.OLIVE_PLACED_KEY)),
                 GenerationStep.Decoration.VEGETAL_DECORATION));
 
@@ -35,7 +43,7 @@ public class ModBiomeModifiers {
                 GenerationStep.Decoration.VEGETAL_DECORATION));
 
         context.register(ADD_ACONITE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
-                biomes.getOrThrow(Tags.Biomes.IS_MUSHROOM),
+                biomes.getOrThrow(Tags.Biomes.IS_MOUNTAIN),
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.ACONITE_PLACED_KEY)),
                 GenerationStep.Decoration.VEGETAL_DECORATION));
 
@@ -44,6 +52,21 @@ public class ModBiomeModifiers {
                 HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.LAUREL_PLACED_KEY)),
                 GenerationStep.Decoration.VEGETAL_DECORATION));
 
+        context.register(SPAWN_RACOON, new ForgeBiomeModifiers.AddSpawnsBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_PLAINS),
+                List.of(new MobSpawnSettings.SpawnerData(ModEntities.RACOON.get(), 40, 1, 6))));
+
+        context.register(SPAWN_CYCLOP, new ForgeBiomeModifiers.AddSpawnsBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_MOUNTAIN),
+                List.of(new MobSpawnSettings.SpawnerData(ModEntities.CYCLOPS.get(), 5, 1, 1))));
+
+        context.register(SPAWN_LYNX, new ForgeBiomeModifiers.AddSpawnsBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_MOUNTAIN),
+                List.of(new MobSpawnSettings.SpawnerData(ModEntities.BALKANLYNX.get(), 20, 1, 2))));
+
+        context.register(SPAWN_MINOTAUR, new ForgeBiomeModifiers.AddSpawnsBiomeModifier(
+                biomes.getOrThrow(Tags.Biomes.IS_MOUNTAIN),
+                List.of(new MobSpawnSettings.SpawnerData(ModEntities.MINOTAUR.get(), 5, 1, 1))));
     }
 
 
