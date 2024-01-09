80001ece68c33f746c8c3b87bde8e4fda25c9ea2
NataliaNNH <s26220@pjwstk.edu.pl>
2024-01-10 00:09:29 +0100

    Dodanie nowych trade'ów z Gods Smith

--- src/main/java/net/mmmteam/minecraftmythologicalmod/event/ModEvents.java
+++ src/main/java/net/mmmteam/minecraftmythologicalmod/event/ModEvents.java
@@ -22,15 +22,52 @@ public class ModEvents {
 
         if(event.getType() == ModVillager.ModVillagers.GODS_SMITH.get()) {
             Int2ObjectMap<List<VillagerTrades.ItemListing>> trades = event.getTrades();
-            ItemStack stack = new ItemStack(ModItems.EXCALIBUR.get(), 1);
-            int villagerLevel = 1;
 
+            ItemStack stack = new ItemStack(Items.BRUSH, 1);
+            int villagerLevel = 1;
             trades.get(villagerLevel).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.EMERALD, 25), stack, 2, 5, 0.06f
+                    new ItemStack(Items.EMERALD, 3), stack, 2, 5, 0.06f
+            ));
+            trades.get(villagerLevel).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(ModBlocks.ACONITE.get()), 3, 2, 0.02f
+            ));
+            trades.get(villagerLevel).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(ModBlocks.IRIS.get()), 3, 2, 0.02f
+            ));
+            trades.get(villagerLevel).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(Items.ARCHER_POTTERY_SHERD), 3, 2, 0.02f
+            ));
+
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(ModItems.AMBROSIA.get()), 3, 2, 0.02f
+            ));
+
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(ModItems.IRIS_PYXIS.get()), 3, 2, 0.02f
+            ));
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(Items.HEART_OF_THE_SEA), 3, 2, 0.02f
+            ));
+            trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 5),
+                    new ItemStack(Items.WITHER_ROSE), 3, 2, 0.02f
+            ));
+            trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 20),
+                    new ItemStack(ModItems.EXCALIBUR.get()), 3, 2, 0.02f
+            ));
+            trades.get(3).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemStack(Items.EMERALD, 20),
+                    new ItemStack(ModItems.SPARTAN_SHIELD.get()), 3, 2, 0.02f
             ));
         }
     }
-
 }
 
 
