b308788e1f1befa320ffd632829f7e1bfc4a5683
NataliaNNH <s26220@pjwstk.edu.pl>
2024-01-16 23:51:20 +0100

    Uprawa ryżu

--- src/main/java/net/mmmteam/minecraftmythologicalmod/block/custom/RiceCropBlock.java
+++ src/main/java/net/mmmteam/minecraftmythologicalmod/block/custom/RiceCropBlock.java
@@ -1,23 +1,10 @@
 package net.mmmteam.minecraftmythologicalmod.block.custom;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.ItemLike;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.common.IPlantable;
 import net.mmmteam.minecraftmythologicalmod.item.ModItems;
 
 public class RiceCropBlock extends CropBlock {
