da9310e84e614a13bf49536b6a6295942cd081af
NataliaNNH <s26220@pjwstk.edu.pl>
2024-01-16 23:49:41 +0100

    Uprawa ryżu

---
+++ src/main/java/net/mmmteam/minecraftmythologicalmod/block/custom/RiceCropBlock.java
@@ -0,0 +1,50 @@
+package net.mmmteam.minecraftmythologicalmod.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.common.IPlantable;
+import net.mmmteam.minecraftmythologicalmod.item.ModItems;
+
+public class RiceCropBlock extends CropBlock {
+    public static final int MAX_AGE = 4;
+    public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 4);
+
+    public RiceCropBlock(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public ItemLike getBaseSeedId() {
+        return ModItems.RICE_SEEDS.get();
+    }
+
+    @Override
+    public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(AGE);
+    }
+}
