fbe1437d79434e28fe84d9a9011bb2e9f2ff9fd8
NataliaNNH <s26220@pjwstk.edu.pl>
2024-01-23 01:08:14 +0100

    Naprawiona Rybka Koi

--- src/main/java/net/mmmteam/minecraftmythologicalmod/entity/custom/KoiFishEntity.java
+++ src/main/java/net/mmmteam/minecraftmythologicalmod/entity/custom/KoiFishEntity.java
@@ -4,23 +4,10 @@ import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.*;
-
-import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl;
-import net.minecraft.world.entity.ai.goal.*;
-import net.minecraft.world.entity.ai.navigation.PathNavigation;
-import net.minecraft.world.entity.ai.navigation.WaterBoundPathNavigation;
 import net.minecraft.world.entity.animal.AbstractSchoolingFish;
-import net.minecraft.world.entity.animal.Animal;
-import net.minecraft.world.entity.animal.WaterAnimal;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.pathfinder.BlockPathTypes;
-import net.minecraft.world.phys.Vec3;
-import net.mmmteam.minecraftmythologicalmod.entity.ai.AnimalAISwimBottom;
 
 public class KoiFishEntity extends AbstractSchoolingFish {
     public KoiFishEntity(EntityType<? extends KoiFishEntity> p_27523_, Level p_27524_) {
