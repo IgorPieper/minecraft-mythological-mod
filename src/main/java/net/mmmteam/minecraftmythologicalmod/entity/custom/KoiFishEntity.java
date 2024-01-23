8b505d5f134070afc1d4a8a54334ba7d2e177768
NataliaNNH <s26220@pjwstk.edu.pl>
2024-01-23 01:07:49 +0100

    Naprawiona Rybka Koi

--- src/main/java/net/mmmteam/minecraftmythologicalmod/entity/custom/KoiFishEntity.java
+++ src/main/java/net/mmmteam/minecraftmythologicalmod/entity/custom/KoiFishEntity.java
@@ -11,121 +11,43 @@ import net.minecraft.world.entity.ai.control.SmoothSwimmingLookControl;
 import net.minecraft.world.entity.ai.goal.*;
 import net.minecraft.world.entity.ai.navigation.PathNavigation;
 import net.minecraft.world.entity.ai.navigation.WaterBoundPathNavigation;
+import net.minecraft.world.entity.animal.AbstractSchoolingFish;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.WaterAnimal;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.phys.Vec3;
 import net.mmmteam.minecraftmythologicalmod.entity.ai.AnimalAISwimBottom;
 
-public class KoiFishEntity extends WaterAnimal {
-    public final AnimationState idleAnimationState = new AnimationState();
-    private int idleAnimationTimeout = 0;
-
-    public KoiFishEntity(EntityType<? extends WaterAnimal> pEntityType, Level pLevel) {
-        super(pEntityType, pLevel);
-        this.setPathfindingMalus(BlockPathTypes.WATER, 0.0F);
-        this.lookControl = new SmoothSwimmingLookControl(this, 10);
+public class KoiFishEntity extends AbstractSchoolingFish {
+    public KoiFishEntity(EntityType<? extends KoiFishEntity> p_27523_, Level p_27524_) {
+        super(p_27523_, p_27524_);
     }
 
-    public boolean canBreatheUnderwater() {
-        return true;
+    public int getMaxSchoolSize() {
+        return 5;
     }
 
-    public MobType getMobType() {
-        return MobType.WATER;
+    public ItemStack getBucketItemStack() {
+        return new ItemStack(Items.SALMON_BUCKET);
     }
 
-    public boolean isPushedByFluid() {
-        return false;
+    protected SoundEvent getAmbientSound() {
+        return SoundEvents.SALMON_AMBIENT;
     }
 
-    public boolean canBeLeashed(Player pPlayer) {
-        return false;
-    }
-
-
-    @Override
-    protected void registerGoals() {
-     //   this.goalSelector.addGoal(0, new RandomSwimmingGoal(this, 1.0D, 120));
-        this.goalSelector.addGoal(1, new TryFindWaterGoal(this));
-        this.goalSelector.addGoal(2, new PanicGoal(this, 1));
-        this.goalSelector.addGoal(3, new AnimalAISwimBottom(this, 1F, 7));
-    }
-    public void travel(Vec3 travelVector) {
-        if (this.isEffectiveAi() && this.isInWater()) {
-            this.moveRelative(this.getSpeed(), travelVector);
-            this.move(MoverType.SELF, this.getDeltaMovement());
-            this.setDeltaMovement(this.getDeltaMovement().scale(0.9D));
-            if (this.getTarget() == null) {
-                this.setDeltaMovement(this.getDeltaMovement().add(0.0D, 0.001D, 0.0D));
-            }
-        } else {
-            super.travel(travelVector);
-        }
-
-    }
-
-    public static AttributeSupplier.Builder createAttributes() {
-        return Animal.createLivingAttributes()
-                .add(Attributes.MAX_HEALTH, 35)
-                .add(Attributes.MOVEMENT_SPEED, 0.12f)
-                .add(Attributes.FOLLOW_RANGE, 10);
-    }
-    protected PathNavigation createNavigation(Level worldIn) {
-        return new WaterBoundPathNavigation(this, worldIn);
-    }
-    @Override
-    protected void handleAirSupply(int p_209207_1_) {
-        if (this.isAlive() && !this.isInWaterOrBubble()) {
-            this.setAirSupply(p_209207_1_ - 1);
-            if (this.getAirSupply() == -20) {
-                this.setAirSupply(0);
-                this.hurt(damageSources().drown(), random.nextInt(2) == 0 ? 1F : 0F);
-            }
-        } else {
-            this.setAirSupply(2000);
-        }
-    }
-
-
-    private void setupAnimationStates() {
-        if (this.idleAnimationTimeout <= 0) {
-            this.idleAnimationTimeout = this.random.nextInt(40) + 80;
-            this.idleAnimationState.start(this.tickCount);
-        } else {
-            --this.idleAnimationTimeout;
-        }
-    }
-
-    protected void updateWalkAnimation(float v) {
-        float f;
-        if (this.getPose() == Pose.STANDING) {
-            f = Math.min(v * 6.0F, 1.0F);
-        } else {
-            f = 0.0F;
-        }
-
-        this.walkAnimation.update(f, 0.2F);
-    }
-
-    @Override
-    public void tick() {
-        super.tick();
-
-        if (this.level().isClientSide()) {
-            this.setupAnimationStates();
-        }
-    }
     protected SoundEvent getDeathSound() {
         return SoundEvents.SALMON_DEATH;
     }
-    protected SoundEvent getHurtSound(DamageSource damageSourceIn) {
+
+    protected SoundEvent getHurtSound(DamageSource p_29795_) {
         return SoundEvents.SALMON_HURT;
     }
-    protected SoundEvent getSwimSound() {
-        return SoundEvents.FISH_SWIM;
+
+    protected SoundEvent getFlopSound() {
+        return SoundEvents.SALMON_FLOP;
     }
 }
-
